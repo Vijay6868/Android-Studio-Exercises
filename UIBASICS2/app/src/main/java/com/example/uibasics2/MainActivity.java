b435735d24ec652382a21e8995efa1e9249f3cf5
Vijay6868 <saroyav3@gmail.com>
2023-12-01 22:20:28 +1300

    2-working on UIBASIC2

--- UIBASICS2/app/src/main/java/com/example/uibasics2/MainActivity.java
+++ UIBASICS2/app/src/main/java/com/example/uibasics2/MainActivity.java
@@ -5,26 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.btnHello) {
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btnHello = findViewById(R.id.btnHello);
-        btnHello.setOnClickListener(this );
+        btnHello.setOnClickListener(this);
+
+        btnHello.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Long press", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        EditText edTextName = findViewById(R.id.edTxtName);
+
     }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getid()){
-            case R.id.btnHello:
-                System.out.println("Hello");
-                break;
-            default:
-                break;
-        }
-    }
+
 }
