c76513936c218abe906e9caa3209dfaaba2ba533
Vijay6868 <saroyav3@gmail.com>
2023-12-01 20:56:50 +1300

    understanding interface

--- UIBASICS2/app/src/main/java/com/example/uibasics2/MainActivity.java
+++ UIBASICS2/app/src/main/java/com/example/uibasics2/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnHello = findViewById(R.id.btnHello);
-        btnHello.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("yayyy");
-            }
-        });
+        btnHello.setOnClickListener(this );
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getid()){
+            case R.id.btnHello:
+                System.out.println("Hello");
+                break;
+            default:
+                break;
+        }
     }
 }
