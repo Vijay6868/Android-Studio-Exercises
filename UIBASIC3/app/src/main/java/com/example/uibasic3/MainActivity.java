ca5ac5ec58cf0f7ebd0866fd36349bf1302937d9
Vijay6868 <saroyav3@gmail.com>
2023-12-03 12:53:03 +1300

    UIBASIC3 UIBASIC3 functionality added to checkboxes

--- UIBASIC3/app/src/main/java/com/example/uibasic3/MainActivity.java
+++ UIBASIC3/app/src/main/java/com/example/uibasic3/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.uibasic3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+     private CheckBox checkboxHarry, checkboxMatrix, checkboxJoker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkboxHarry = findViewById(R.id.checkboxHarry);
+        checkboxMatrix = findViewById(R.id.checkboxMatrix);
+        checkboxJoker = findViewById(R.id.checkboxJoker);
+
+        checkboxHarry.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this, "you watched harry potter", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "you need to watch Harry potter", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
