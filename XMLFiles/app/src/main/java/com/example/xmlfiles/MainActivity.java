2c7081a4886a642bfda23f41aa95229113d22164
Vijay6868 <saroyav3@gmail.com>
2023-12-09 20:44:48 +1300

    finished with XML files

--- XMLFiles/app/src/main/java/com/example/xmlfiles/MainActivity.java
+++ XMLFiles/app/src/main/java/com/example/xmlfiles/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.xmlfiles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
 
         txtHello = findViewById(R.id.txtHello);
         txtHello.setText(getString(R.string.hello));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings_menu
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
