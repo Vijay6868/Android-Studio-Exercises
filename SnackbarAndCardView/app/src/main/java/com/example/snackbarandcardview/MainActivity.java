84b66698cec02a886ff3ed114527b7a321119468
Vijay6868 <saroyav3@gmail.com>
2023-12-20 12:53:05 +1300

    finished with card

--- SnackbarAndCardView/app/src/main/java/com/example/snackbarandcardview/MainActivity.java
+++ SnackbarAndCardView/app/src/main/java/com/example/snackbarandcardview/MainActivity.java
@@ -9,11 +9,13 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnSnackBar;
     private RelativeLayout RLlayout;
+    private MaterialCardView mcw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
         btnSnackBar =findViewById(R.id.btSnackBar);
         RLlayout = findViewById(R.id.RLlayout);
+
+        mcw=findViewById(R.id.mcw);
+        mcw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Card clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
         btnSnackBar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
