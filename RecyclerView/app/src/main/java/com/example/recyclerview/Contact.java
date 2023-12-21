b90ec736db721c54035e5ed429c29b70fdf17155
Vijay6868 <saroyav3@gmail.com>
2023-12-22 11:09:07 +1300

    finished first part of recycler view

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Contact.java
@@ -0,0 +1,44 @@
+package com.example.recyclerview;
+
+public class Contact {
+    private String name;
+    private String email;
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+
+    public Contact(String name) {
+        this.name = name;
+    }
+}
---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/ContactRecViewAdapter.java
@@ -0,0 +1,50 @@
+package com.example.recyclerview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactRecViewAdapter extends RecyclerView.Adapter<ContactRecViewAdapter.ViewHolder>{
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public ContactRecViewAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(ArrayList<Contact> contacts) {
+        this.contacts = contacts;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView txtName;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.txtName);
+        }
+    }
+}
